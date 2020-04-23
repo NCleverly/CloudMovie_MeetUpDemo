3b47eb547d71b8d63e2b78e55c6f42db4e5098e8
Nate Cleverly <nate_cleverly@hotmail.com>
2020-04-23 12:22:21 -0700

    Initial check-in of module CloudMovie.Mobile

---
+++ CloudMovie.Mobile/Models/SomeObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Core;
+
+namespace CloudMovie.Mobile
+{
+    public class SomeObject : CoreModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
