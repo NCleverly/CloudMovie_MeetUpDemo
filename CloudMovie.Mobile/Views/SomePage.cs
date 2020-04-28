f49bcf60c0c6477a234824fb33ccc239160649ac
Nate Cleverly <rytan2005@gmail.com>
2020-04-28 14:52:13 -0700

    Some more fixes

--- CloudMovie.Mobile/Views/SomePage.cs
+++ CloudMovie.Mobile/Views/SomePage.cs
@@ -16,43 +16,13 @@ namespace CloudMovie.Mobile
             {
                 Padding = 20,
                 Children = {
-                    new Label()
-                    {
-                        Text="Enter Text:",
-                        Margin = new Thickness(5, 5, 5, 0)
-                    },
-                    new CoreMaskedEntry()
-                    {
-                        Margin = 5,
-                        BackgroundColor = Color.Transparent
-                    }.Bind(CoreMaskedEntry.TextProperty, nameof(SomeViewModel.SomeText)),
-                    new Label()
-                    {
-                        Margin = 5
-                    }.Bind(Label.TextProperty, nameof(SomeViewModel.SomeText), converter: CoreSettings.UpperText),
-                    new CoreButton()
-                    {
-                        Text="Some Action",
-                        Style = CoreStyles.LightOrange,
-                        Margin=5,
-                    }.Bind(Button.CommandProperty,nameof(SomeViewModel.SomeAction)),
-                    new Label()
-                    {
-                        Margin = 5
-                    }.Bind(Label.TextProperty, nameof(SomeViewModel.TotalItems), stringFormat: "Total count is {0}"),
-                    new CoreButton()
-                    {
-                        Text="See Fonts",
-                        Style = CoreStyles.LightOrange,
-                        Margin=5,
-                    }.Bind(Button.CommandProperty,nameof(SomeViewModel.SeeFontAction)),
                     new Grid{
                         ColumnDefinitions = new ColumnDefinitionCollection{
                             new ColumnDefinition {Width = new GridLength(1, GridUnitType.Star) },
                             new ColumnDefinition {Width = new GridLength(1, GridUnitType.Star) },
                         },
                         RowDefinitions = new RowDefinitionCollection{
-                            new RowDefinition{Height=new GridLength(1, GridUnitType.Star)}
+                            new RowDefinition{Height=new GridLength(50)}
                         },
                         Children = {
                             new CoreButton()
